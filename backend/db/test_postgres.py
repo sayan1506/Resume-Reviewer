2f6d4ec54bff9d4310ebddd1eab79ba937f7d6be
Sayan Dutta <sayandutta71504@gmail.com>
2026-03-10 23:47:50 +0530

    removing some files

--- backend/db/test_postgres.py
+++
@@ -1,8 +0,0 @@
-from backend.db.postgres import engine
-
-try:
-    conn = engine.connect()
-    print("PostgreSQL connection successful")
-    conn.close()
-except Exception as e:
-    print("PostgreSQL connection failed:", e)
