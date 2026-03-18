fb9762fea61095720b31458fe21608aad22f2a40
Sayan Dutta <sayandutta71504@gmail.com>
2026-03-18 09:09:09 +0530

    Deployment changes

--- backend/ai/ChatGpt5.py
+++ backend/ai/ChatGpt5.py
@@ -1,33 +1,33 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-from azure.ai.inference import ChatCompletionsClient
-from azure.ai.inference.models import SystemMessage, UserMessage
-from azure.core.credentials import AzureKeyCredential
-
-
-load_dotenv()
-
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-
-endpoint = "https://models.github.ai/inference"
-model = "openai/gpt-5"
+# from azure.ai.inference import ChatCompletionsClient
+# from azure.ai.inference.models import SystemMessage, UserMessage
+# from azure.core.credentials import AzureKeyCredential
 
 
-client = ChatCompletionsClient(
-    endpoint=endpoint,
-    credential=AzureKeyCredential(GITHUB_TOKEN),
-)
+# load_dotenv()
+
+# GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+# endpoint = "https://models.github.ai/inference"
+# model = "openai/gpt-5"
 
 
-def generate_ai_response(prompt: str):
+# client = ChatCompletionsClient(
+#     endpoint=endpoint,
+#     credential=AzureKeyCredential(GITHUB_TOKEN),
+# )
 
-    response = client.complete(
-        model=model,
-        messages=[
-            SystemMessage("You are an expert AI assistant."),
-            UserMessage(prompt),
-        ],
-    )
 
-    return response.choices[0].message.content
+# def generate_ai_response(prompt: str):
+
+#     response = client.complete(
+#         model=model,
+#         messages=[
+#             SystemMessage("You are an expert AI assistant."),
+#             UserMessage(prompt),
+#         ],
+#     )
+
+#     return response.choices[0].message.content
