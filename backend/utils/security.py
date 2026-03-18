e50b189741e92229c386a87ce7c4aa4fd432f04f
Sayan Dutta <sayandutta71504@gmail.com>
2026-03-18 10:42:23 +0530

    requirmnet file change

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,13 +1,12 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+pwd_context = CryptContext(
+    schemes=["pbkdf2_sha256"],
+    deprecated="auto"
+)
 
 def hash_password(password: str):
-    password = password[:72]   # enforce bcrypt limit
     return pwd_context.hash(password)
 
-
 def verify_password(plain_password, hashed_password):
-    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
