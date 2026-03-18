197af8cb413092f0a74a5f95dcdaffe62ef91a7e
Sayan Dutta <sayandutta71504@gmail.com>
2026-03-18 10:17:46 +0530

    frontend deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://resume-reviewer-yqdp.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
